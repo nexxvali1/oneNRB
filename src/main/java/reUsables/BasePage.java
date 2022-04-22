84f04bba89292704bad5998fb91113fb2953974f
Mukit <Mukit@syeds-air.attlocal.net>
2022-04-22 10:22:17 -0500

    Base Page added

--- src/main/java/reUsables/BasePage.java
+++ src/main/java/reUsables/BasePage.java
@@ -1,14 +1,29 @@
 package reUsables;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class BasePage {
 
-	protected WebDriver driver;	
-	
+	static String ssLocation = "/Users/Mukit/Documents/workspace/nexxvali/screenShots/TestNexx.jpeg";
+
+	protected WebDriver driver;
+
 	public BasePage(WebDriver driver) {
 		this.driver = driver;
-		
 	}
 
+	public void ifTestFailes() {
+		TakesScreenshot ss = (TakesScreenshot) driver;
+
+		try {
+			FileUtils.copyFile(ss.getScreenshotAs(OutputType.FILE), new File(ssLocation));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ src/main/java/reUsables/CustomListener.java
@@ -0,0 +1,13 @@
+package reUsables;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import reUsables.BasePage;
+
+public class CustomListener extends BasePage {
+
+}
